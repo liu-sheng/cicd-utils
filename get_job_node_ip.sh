ff8edd30c2dc5a02808caf9810b21d5934547e8b
liusheng <liusheng@huawei.com>
2018-01-31 03:08:49 +0000

    update scripts

--- get_job_node_ip.sh
+++ get_job_node_ip.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# This script should be run in the zuul server
-
 job_id=$1
 if [ -z "$job_id" ];then
     echo 'please sepcify job id'
     exit 1
 fi
-line==$(grep "To git+ssh" /var/lib/zuul/builds/$job_id/work/logs/job-output.txt)
-str=${line#*@}
-echo ${str%//*}
+node_line=$(grep "$job_id" /var/log/zuul/zuul.log |grep "Execute job")
+ip_line==$(grep "To git+ssh" /var/lib/zuul/builds/$job_id/work/logs/job-output.txt)
+
+node_str=${node_line#*Node }
+echo ${node_str%% [*}
+ip_str=${ip_line#*@}
+echo ${ip_str%//*}
--- restart_nodepool.sh
+++ restart_nodepool.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-for pid in `ps -ef |grep /usr/local/bin/nodepool-luancher |grep -v grep | awk '{print $2}'`
+for pid in `ps -ef |grep /usr/local/bin/nodepool-builder |grep -v grep | awk '{print $2}'`
 do
     kill -9 $pid
 done
-for pid in `ps -ef |grep /usr/local/bin/nodepool-luancher |grep -v grep | awk '{print $2}'`
+for pid in `ps -ef |grep /usr/local/bin/nodepool-launcher |grep -v grep | awk '{print $2}'`
 do
     kill -9 $pid
 done
+sleep 2
 nohup /usr/bin/python3 /usr/local/bin/nodepool-builder -d -l /etc/nodepool/builder-logging.conf -c /etc/nodepool/nodepool.yaml > /dev/null 2>&1 & 
 nohup /usr/bin/python3 /usr/local/bin/nodepool-launcher -d -l /etc/nodepool/launcher-logging.conf -c /etc/nodepool/nodepool.yaml > /dev/null 2>&1 &
